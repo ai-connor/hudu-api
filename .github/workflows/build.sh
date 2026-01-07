3124b271b95cfc5c058e7a4dae497f11e6ffdf5e
Scott Phillips <polygonguru@gmail.com>
2026-01-07 16:50:09 +0000

    feat: HUDU Api

---
+++ .github/workflows/build.sh
@@ -0,0 +1,13 @@
+# TODO - Ensure openapi tools are installed:
+npm install @openapitools/openapi-generator-cli -g
+openapi-generator-cli generate \
+    -i ../../_hudu-api.json \
+    -g go \
+    -o ../../ \
+    -c config.yaml \
+    --skip-validate-spec \
+    --git-user-id ai-connor \
+    --git-repo-id hudu-api
+
+cd ../..
+go fmt
---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/ai-connor/hudu-api
+
+go 1.23
+
+require (
+)
